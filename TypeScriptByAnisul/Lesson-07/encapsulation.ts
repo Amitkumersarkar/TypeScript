7daec4be69c258d7bec9c950b8089199e79d3177
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-11-24 23:25:09 +0600

    updated file

--- TypeScriptByAnisul/Lesson-07/encapsulation.ts
+++ TypeScriptByAnisul/Lesson-07/encapsulation.ts
@@ -5,21 +5,25 @@ class User {
     constructor(userName: string, age: number) {
         this.userName = userName;
         this.age = age;
-
     }
+
     display(): void {
-        console.log('Hello');
-    };
+        console.log("Hello");
+    }
 }
 
 class Student extends User {
-    constructor(userName: string, age: number, studentId: number)
-    super(userName, age);
-this.studentId = StudentId;
-}
-display(): void {
-    console.log(`userName:${this.userName},age:${this.age},id:${this.studentId}`)
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number) {
+        super(userName, age);
+        this.studentId = studentId;
+    }
+
+    display(): void {
+        console.log(`userName: ${this.userName}, age: ${this.age}, id: ${this.studentId}`);
+    }
 }
 
-let student1 = new Student("Amit", 51, 22235103283);
+let student1 = new Student("Amit", 23, 22235103283);
 student1.display();
